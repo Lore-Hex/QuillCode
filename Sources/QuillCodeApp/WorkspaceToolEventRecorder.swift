b9a7e4e68603c474b2ba7523379843c59b95d048
Joseph Perla <gh@jperla.com>
2026-06-24 09:40:10 -0700

    Move tool execution recording into recorder

--- Sources/QuillCodeApp/WorkspaceToolEventRecorder.swift
+++ Sources/QuillCodeApp/WorkspaceToolEventRecorder.swift
@@ -29,4 +29,11 @@ struct WorkspaceToolEventRecorder {
     static func append(call: ToolCall, result: ToolResult, to thread: inout ChatThread) {
         thread.events.append(contentsOf: events(call: call, result: result))
     }
+
+    static func append(execution: WorkspaceToolCallExecution, to thread: inout ChatThread) {
+        append(call: execution.primary.call, result: execution.primary.result, to: &thread)
+        for followUp in execution.followUps {
+            append(call: followUp.call, result: followUp.result, to: &thread)
+        }
+    }
 }
